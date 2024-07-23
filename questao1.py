ab47a86e11890b9b610da4a9faf37daad8cf1ceb
davidsamuel-dev <davidsamuel.dev@gmail.com>
2024-07-23 10:20:33 -0300

    teste

---
+++ questao1.py
@@ -0,0 +1,16 @@
+# Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
+# numero = []
+
+# for x in range(0,5):
+#     n = input("Insira o ", x , "° número")
+#     numero.append(n)
+# print(numero)
+
+for x in range(0, 10):
+    print("Teste", x+1)
+
+print("\nFim")
+
+numero = []
+
+numero.append(input("Insira um número: "))
